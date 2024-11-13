b7be500f302774e5ffffa3800e90eedffeed623d
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-11-13 14:36:39 -0600

    initial sample

---
+++ src/EventPublisher/Program.cs
@@ -0,0 +1,53 @@
+﻿using EventPublisher;
+using Marten;
+using Marten.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+var builder = Host.CreateApplicationBuilder();
+
+
+builder.Logging.AddConsole();
+
+builder.Logging.AddOpenTelemetry(logging =>
+{
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
+    
+});
+
+
+builder.Services.AddOpenTelemetry()
+    .UseOtlpExporter()
+    
+    // Enable exports of Open Telemetry activities for Marten
+    .WithTracing(tracing =>
+    {
+        tracing.AddSource("Marten");
+        tracing.AddZipkinExporter();
+    })
+
+    // Enable exports of metrics for Marten
+    .WithMetrics(metrics =>
+    {
+        metrics.AddMeter("Marten");
+    });
+
+builder.Services.AddHostedService<HostedPublisher>();
+
+builder.Services.AddMarten(opts =>
+    {
+        opts.DatabaseSchemaName = "cli";
+
+        // Turn on Otel tracing for connection activity, and
+        // also tag events to each span for all the Marten "write"
+        // operations
+        opts.OpenTelemetry.TrackConnections = TrackLevel.Verbose;
+
+        opts.OpenTelemetry.TrackEventCounters();
+    });
+
+builder.Build().Run();
---
+++ src/TripBuildingService/Program.cs
@@ -0,0 +1,56 @@
+﻿using Marten;
+using Marten.Events.Daemon.Resiliency;
+using Marten.Events.Projections;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Logging.AddOpenTelemetry(logging =>
+{
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
+});
+
+builder.Services.AddOpenTelemetry().UseOtlpExporter();
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing =>
+    {
+        tracing.AddSource("Marten");
+        tracing.AddZipkinExporter();
+    })
+    .WithMetrics(metrics =>
+    {
+        metrics.AddMeter("Marten");
+        metrics.AddOtlpExporter(opts =>
+        {
+            opts.Endpoint = new Uri("http://localhost:9090/api/v1/otlp/v1/metrics");
+            opts.Protocol = OtlpExportProtocol.HttpProtobuf;
+        });
+    });
+
+builder.Services.AddMarten(opts =>
+{
+    opts.DatabaseSchemaName = "cli";
+
+    // Register all event store projections ahead of time
+    opts.Projections
+        .Add(new TripProjection(), ProjectionLifecycle.Async);
+
+    opts.Projections
+        .Add(new DayProjection(), ProjectionLifecycle.Async);
+
+    opts.Projections
+        .Add(new DistanceProjection(), ProjectionLifecycle.Async);
+}).AddAsyncDaemon(DaemonMode.Solo)
+    
+    // Use PostgreSQL data source from the IoC container
+    .UseNpgsqlDataSource();
+
+await builder.Build().RunAsync();
